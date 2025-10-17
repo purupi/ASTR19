d8c6fa7b185f48a51cecb8b6e12c14154a340b4f
purupi <pupatil@ucsc.edu>
2025-10-16 18:13:41 -0700

    Add files via upload
    py script for Code Journal 1, assignment 1

---
+++ Intro.py
@@ -0,0 +1,14 @@
+def PrintIntro():
+	print("Name: Purvi Patil")
+	
+	print("Prounouns: she/her")
+	
+	print("Favorite Movie: My favorite movie is a Bollywood movie called '3 Idiots.' It's about three college friends meeting after 10 years.")
+
+	print("My favorite food is paani puri. It is an Indian street food.")
+
+def main():
+	PrintIntro()
+
+if __name__ == '__main__':
+	main()
